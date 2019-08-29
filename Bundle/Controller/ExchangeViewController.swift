7b70344773a2cd00a5c93b74fd0ab0b06fea8e72
patrick Wiley <wileypatrick36@yahoo.com>
2019-08-29 18:30:02 +0200

    create ExchangeViewControiller/TranslatorViewControiller/WeatherViewControiller

---
+++ Bundle/Controller/ExchangeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExchangeViewController.swift
+//  Bundle
+//
+//  Created by Patrick Wiley on 29.08.19.
+//  Copyright © 2019 Patrick Wiley. All rights reserved.
+//
+
+import UIKit
+
+class ExchangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
