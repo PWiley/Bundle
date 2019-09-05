27710a6962baf832ed1d816d07c84a1152234737
patrick Wiley <wileypatrick36@yahoo.com>
2019-09-05 14:01:40 +0200

    add ParametersModel and ParametersViewController

---
+++ Bundle/Model/ParametersModel.swift
@@ -0,0 +1,16 @@
+//
+//  ParametersModel.swift
+//  Bundle
+//
+//  Created by Patrick Wiley on 05.09.19.
+//  Copyright © 2019 Patrick Wiley. All rights reserved.
+//
+
+import Foundation
+
+
+class ParametersModel {
+    
+    
+    
+}
