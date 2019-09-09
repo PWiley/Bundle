336ae31ba67293fb777b869b1178a12a6aca0a10
patrick Wiley <wileypatrick36@yahoo.com>
2019-09-09 13:48:15 +0200

    Add LanguageCell/TranslatorViewController/Translate

--- Bundle/Controller/TranslatorViewController.swift
+++ Bundle/Controller/TranslatorViewController.swift
@@ -8,23 +8,55 @@
 
 import UIKit
 
-class TranslatorViewController: UITableViewController {
-
+class TranslatorViewController: ViewController {
+    
+    var translate = Translate()
+    
+    @IBOutlet var tableViewTranslator: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableViewTranslator.estimatedRowHeight = 200
+        translate.languages = translate.createLanguage()
+        
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        tableViewTranslator.backgroundView = UIImageView(image: UIImage(named: "Background_Translator"))
+        tableViewTranslator.invalidateIntrinsicContentSize()
     }
-    */
 
+   
+
+}
+
+extension TranslatorViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return translate.languages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let language = translate.languages[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LanguageCell") as! LanguageCell
+        
+        cell.setLanguage(language: language)
+        return cell
+    }
+//    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+//        if (text == "\n")
+//        {
+//            
+//            self.view.endEditing(true);
+//            return false;
+//        }
+//        return true
+//    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+        
+    }
 }
